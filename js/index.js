3f27b243f84d3cfcfacaa2a2da531f8aaf912365
Boffeer <41575886+Boffeer@users.noreply.github.com>
2021-10-12 23:01:23 +0300

    fix sliders height

--- js/index.js
+++ js/index.js
@@ -52,200 +52,183 @@ const techesSlider = new Swiper('.s3-teches-slider', {
     },
   }
 })
-
-  /**
-   * DROPDOWN
-   */
-  // $(".faq-list-card-top").each(function () {
-  //   $(this).on("click", function () {
-  //     $(this).toggleClass("faq-list-card-top--opened");
-  //     $(this).parent().find(".faq-list-card-bottom").slideToggle();
-  //   });
-  // });
-
-  // const bundlesSlider = new Swiper('.s13-bundles-slider', {
-  //   spaceBetween: 50,
-  //   pagination: {
-  //     el: '.s13-buttons-pagination',
-  //   },
-  //   navigation: {
-  //     nextEl: '.s13-slider-button--next',
-  //     prevEl: '.s13-slider-button--prev',
-  //   },
-  //   breakpoints: {
-  //     // when window width is >= 320px
-  //     1199: {
-  //       slidesPerView: 3,
-  //     },
-  //   }
-  // });
-
-  const programmSlider = new Swiper('.s5-programms-slider', {
-    spaceBetween: 50,
-    pagination: {
-      el: '.s5-buttons-pagination',
+const programmSlider = new Swiper('.s5-programms-slider', {
+  spaceBetween: 50,
+  pagination: {
+    el: '.s5-buttons-pagination',
+  },
+  navigation: {
+    nextEl: '.s5-slider-button--next',
+    prevEl: '.s5-slider-button--prev',
+  },
+  breakpoints: {
+    1199: {
+      slidesPerView: 3,
     },
-    navigation: {
-      nextEl: '.s5-slider-button--next',
-      prevEl: '.s5-slider-button--prev',
-    },
-    breakpoints: {
-      1199: {
-        slidesPerView: 3,
-      },
-    }
-  })
-  const founderSlider = new Swiper('.s4-founder-cards', {
-    spaceBetween: 50,
-    pagination: {
-      el: '.s4-founder-cards-pagination',
-    },
-    navigation: {
-      nextEl: '.s4-founder-cards__button--next',
-      prevEl: '.s4-founder-cards__button--prev',
-    },
-    breakpoints: {
-      1199: {
-        slidesPerView: 3,
-      },
-    }
-  })
-  const momsSlider = new Swiper('.s6-moms-cards', {
-    spaceBetween: 50,
-    pagination: {
-      el: '.s6-moms-cards-pagination',
-    },
-    navigation: {
-      nextEl: '.s6-moms-cards__button--next',
-      prevEl: '.s6-moms-cards__button--prev',
-    },
-    breakpoints: {
-      1199: {
-        slidesPerView: 3,
-      },
-    }
-  })
-  const reviewsSlider = new Swiper('.s8-reviews-slider', {
-    spaceBetween: 50,
-    pagination: {
-      el: '.s8-reviews-slider-pagination',
-    },
-    navigation: {
-      prevEl: '.s8-reviews-slider__button--prev',
-      nextEl: '.s8-reviews-slider__button--next',
-    },
-    breakpoints: {
-      1199: {
-        slidesPerView: 3,
-      },
-    }
-  })
-  const easySlider = new Swiper('.s9-easy-cards', {
-    spaceBetween: 50,
-    pagination: {
-      el: '.s9-easy-cards-pagination',
-    },
-    navigation: {
-      nextEl: '.s9-easy-cards__button--next',
-      prevEl: '.s9-easy-cards__button--prev',
-    },
-    breakpoints: {
-      1199: {
-        slidesPerView: 3,
-      },
-    }
-  })
-  const testimonialsSlider = new Swiper('.s12-testimonials-slider', {
-    spaceBetween: 50,
-    pagination: {
-      el: '.s12-testimonials-slider-pagination',
-    },
-    navigation: {
-      nextEl: '.s12-testimonials-slider__button--next',
-      prevEl: '.s12-testimonials-slider__button--prev',
-    },
-    breakpoints: {
-      1199: {
-        slidesPerView: 3,
-      },
-    }
-  })
-  const certsSlider = new Swiper('.s15-cert-slider', {
-    spaceBetween: 50,
-    pagination: {
-      el: '.s15-cert-slider-pagination',
-    },
-    navigation: {
-      nextEl: '.s15-cert-slider__button--next',
-      prevEl: '.s15-cert-slider__button--prev',
-    },
-    breakpoints: {
-      1199: {
-        slidesPerView: 3,
-      },
-    }
-  })
-  const videosSlider = new Swiper('.s7-videos-slider', {
-    spaceBetween: 50,
-    pagination: {
-      el: '.s7-videos-slider-pagination',
-    },
-    navigation: {
-      nextEl: '.s7-videos-slider__button--next',
-      prevEl: '.s7-videos-slider__button--prev',
-    },
-    breakpoints: {
-      1199: {
-        slidesPerView: 3,
-      },
-    }
-  })
-
-  // const SLIDERS_ARRAY = [programmSlider, bundlesSlider];
-  const SLIDERS_ARRAY = [programmSlider, whoSlider, reviewsSlider, techesSlider, founderSlider, momsSlider, easySlider];
-
-  function fixSingleSliderHeight(slider) {
-      slider.$el[0].style.height = 'auto'
-      let currentSlideIndex = slider.realIndex + 1;
-      let currentSlides = slider.slides;
-      let currentSlide;
-      currentSlides.map(slide => {
-        let regex = new RegExp(`^${currentSlideIndex} /`)
-        if (slide.getAttribute('aria-label').match(regex)) {
-          currentSlide = slide;
-        }
-      })
-      setTimeout(() => {
-        let sliderWrapper = currentSlide.parentElement.parentElement;
-        let sliderHeight = currentSlide.clientHeight;
-        sliderWrapper.style.height = `${sliderHeight}px`
-      }, 10)
   }
-
-  function fixSlidersHeight(slider) {
-    slider.on('slideChange', function () {
-      fixSingleSliderHeight(slider);
-    });
+})
+const founderSlider = new Swiper('.s4-founder-cards', {
+  spaceBetween: 50,
+  pagination: {
+    el: '.s4-founder-cards-pagination',
+  },
+  navigation: {
+    nextEl: '.s4-founder-cards__button--next',
+    prevEl: '.s4-founder-cards__button--prev',
+  },
+  breakpoints: {
+    1199: {
+      slidesPerView: 3,
+    },
   }
+})
+const momsSlider = new Swiper('.s6-moms-cards', {
+  spaceBetween: 50,
+  pagination: {
+    el: '.s6-moms-cards-pagination',
+  },
+  navigation: {
+    nextEl: '.s6-moms-cards__button--next',
+    prevEl: '.s6-moms-cards__button--prev',
+  },
+  breakpoints: {
+    1199: {
+      slidesPerView: 3,
+    },
+  }
+})
+const reviewsSlider = new Swiper('.s8-reviews-slider', {
+  spaceBetween: 50,
+  pagination: {
+    el: '.s8-reviews-slider-pagination',
+  },
+  navigation: {
+    prevEl: '.s8-reviews-slider__button--prev',
+    nextEl: '.s8-reviews-slider__button--next',
+  },
+  breakpoints: {
+    1199: {
+      slidesPerView: 3,
+    },
+  }
+})
+const easySlider = new Swiper('.s9-easy-cards', {
+  spaceBetween: 50,
+  pagination: {
+    el: '.s9-easy-cards-pagination',
+  },
+  navigation: {
+    nextEl: '.s9-easy-cards__button--next',
+    prevEl: '.s9-easy-cards__button--prev',
+  },
+  breakpoints: {
+    1199: {
+      slidesPerView: 3,
+    },
+  }
+})
+const testimonialsSlider = new Swiper('.s12-testimonials-slider', {
+  spaceBetween: 50,
+  pagination: {
+    el: '.s12-testimonials-slider-pagination',
+  },
+  navigation: {
+    nextEl: '.s12-testimonials-slider__button--next',
+    prevEl: '.s12-testimonials-slider__button--prev',
+  },
+  breakpoints: {
+    1199: {
+      slidesPerView: 3,
+    },
+  }
+})
+const certsSlider = new Swiper('.s15-cert-slider', {
+  spaceBetween: 50,
+  pagination: {
+    el: '.s15-cert-slider-pagination',
+  },
+  navigation: {
+    nextEl: '.s15-cert-slider__button--next',
+    prevEl: '.s15-cert-slider__button--prev',
+  },
+  breakpoints: {
+    1199: {
+      slidesPerView: 3,
+    },
+  }
+})
+const videosSlider = new Swiper('.s7-videos-slider', {
+  spaceBetween: 50,
+  pagination: {
+    el: '.s7-videos-slider-pagination',
+  },
+  navigation: {
+    nextEl: '.s7-videos-slider__button--next',
+    prevEl: '.s7-videos-slider__button--prev',
+  },
+  breakpoints: {
+    1199: {
+      slidesPerView: 3,
+    },
+  }
+})
 
-  setTimeout(() => {
-    SLIDERS_ARRAY.forEach(slider => {
-      fixSlidersHeight(slider)
-      setTimeout(() => {
-        slider.slideNext();
-        setTimeout(() => {
-          slider.slidePrev();
-        }, 400)
-        console.log('yep')
-      }, 20)
-      // fixSingleSliderHeight(slider)
+// const SLIDERS_ARRAY = [programmSlider, bundlesSlider];
+const SLIDERS_ARRAY = [
+  whoSlider,
+  techesSlider,
+  founderSlider,
+  programmSlider,
+  momsSlider,
+  videosSlider,
+  reviewsSlider,
+  easySlider,
+  certsSlider,
+  testimonialsSlider,
+];
+
+function fixSingleSliderHeight(slider) {
+    slider.$el[0].style.height = 'auto'
+    let currentSlideIndex = slider.realIndex + 1;
+    let currentSlides = slider.slides;
+    let currentSlide;
+    currentSlides.map(slide => {
+      let regex = new RegExp(`^${currentSlideIndex} /`)
+      if (slide.getAttribute('aria-label').match(regex)) {
+        currentSlide = slide;
+      }
     })
-  }, 100)
-  // window.addEventListener('resize', () => {
-  //   // SLIDERS_ARRAY.forEach(slider => {
-  //   //   fixSlidersHeight(slider)
-  //   // })
-  // })
+    setTimeout(() => {
+      let sliderWrapper = currentSlide.parentElement.parentElement;
+      let sliderHeight = currentSlide.clientHeight;
+      sliderWrapper.style.height = `${sliderHeight}px`
+    }, 10)
+}
+
+function fixSlidersHeight(slider) {
+  slider.on('slideChange', function () {
+    fixSingleSliderHeight(slider);
+  });
+}
+
+setTimeout(() => {
+  SLIDERS_ARRAY.forEach(slider => {
+    fixSlidersHeight(slider)
+    setTimeout(() => {
+      slider.slideNext();
+      setTimeout(() => {
+        slider.slidePrev();
+      }, 400)
+      console.log('yep')
+    }, 20)
+    // fixSingleSliderHeight(slider)
+  })
+}, 100)
+// window.addEventListener('resize', () => {
+//   // SLIDERS_ARRAY.forEach(slider => {
+//   //   fixSlidersHeight(slider)
+//   // })
+// })
 
 function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
@@ -304,8 +287,6 @@ if (window.location.href != 'https://ekaterinaschol.ru/default') {
 }
 
 // DROPDOWNS
-// margin-top = -(answer height + 20)
-
 const dropdowns = [...document.querySelectorAll('.s17-faq-cards-card-question')];
 
 const dropdownOpenedClass = 'opened';
@@ -329,7 +310,6 @@ dropdowns.forEach((dropdown, index) => {
 
 
 // YT video and thumbnail getter
-
 const reviews = [...document.querySelectorAll('.s8-reviews-slider-slide')]
 
 function getYtThumbnailUrl(id) {
@@ -338,24 +318,6 @@ function getYtThumbnailUrl(id) {
 function getYtEmbedUrl(id) {
   return `https://www.youtube.com/embed/${id}?autoplay=1&enablejsapi=1`
 }
-
-// reviews.forEach(review => {
-//   let ytVideoId = review.getAttribute('data-yt-id');
-//   let ytThumbnailSrc = getYtThumbnailUrl(ytVideoId);
-//   let ytEmbedSrc = getYtEmbedUrl(ytVideoId);
-
-//   const preview = review.querySelector('.s8-reviews-slider-slide-video__media--preview')
-//   const video = review.querySelector('.s8-reviews-slider-slide-video__media--video')
-
-//   preview.setAttribute('src', ytThumbnailSrc);
-
-//   review.querySelector('.s8-reviews-slider-slide-video__play').addEventListener('click', function() {
-//     video.style.display = 'block';
-//     video.setAttribute('src', ytEmbedSrc);
-//     preview.style.display = 'none';
-//     this.remove();
-//   })
-// })
 
 function ytLazyLoad($) {
   $.cards.forEach(card => {
